e001205ec1e4961b24d6747da4a485c17679b7c5
Иван Горбунов <muervos@muervos.muervos>
2025-07-12 19:19:26 +0300

    start project

---
+++ src/components/TaskList/TaskItem/TaskItem.tsx
@@ -0,0 +1,14 @@
+export const TaskItem = () => {
+    return(
+        <>
+        <h1>заголовок</h1>
+        <p>описание</p>
+        <div>
+            <div>категория</div>
+            <div>Статус</div>
+            <div>Приоритет</div>
+        </div>
+        <button>Редактировать</button>
+        </>
+    )
+}
